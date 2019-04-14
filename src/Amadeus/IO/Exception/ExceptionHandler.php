4e23a2645f09abbd84b17a55672a2068d14bc1ca
ChinoWorth <lixworth@gmail.com>
2019-04-14 23:45:19 +0800

    Update ExceptionHandler.php

--- src/Amadeus/IO/Exception/ExceptionHandler.php
+++ src/Amadeus/IO/Exception/ExceptionHandler.php
@@ -11,7 +11,7 @@ class ExceptionHandler
 {
     public static function onError(Exception $exception)
     {
-        Logger::printLine("Exception: [".$exception->getFile().":".$exception->getLine()."]".$exception->getMessage()." \n <pre>".$exception->getCode()."</pre> \n ".$exception->getTraceAsString(),1);
+        Logger::printLine("Exception: [".$exception->getFile().":".$exception->getLine()."]".$exception->getMessage()." \n <pre>".$exception->getCode()."</pre> \n ".$exception->getTraceAsString(),6);
         return true;
     }
 }

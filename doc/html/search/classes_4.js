a7840c4c0be03e352375a97bef1f66a89647d766
lixworth <lixworth@gmail.com>
2019-04-21 23:32:27 +0800

    Doxygen phpdoc

---
+++ doc/html/search/classes_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['gamecontroller',['GameController',['../classAmadeus_1_1Plugin_1_1Game_1_1GameController.html',1,'Amadeus::Plugin::Game']]],
+  ['gamelistener',['GameListener',['../interfaceAmadeus_1_1Plugin_1_1Listener_1_1GameListener.html',1,'Amadeus::Plugin::Listener']]]
+];

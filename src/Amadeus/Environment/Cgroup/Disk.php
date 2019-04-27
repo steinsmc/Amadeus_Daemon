ee5365e4fca4040161ac91fd74ed7698a62a3066
William Wang <dhdjwjy@gmail.com>
2019-04-26 21:22:31 -0400

    Cgroup Blkio限制完成

--- src/Amadeus/Environment/Cgroup/Disk.php
+++ src/Amadeus/Environment/Cgroup/Disk.php
@@ -12,10 +12,25 @@ class Disk
 {
     /**
      * @param string $c_blkio
-     * @param int $diskSpeed
+     * @param int $primary_id
+     * @param int $secondary_id
+     * @param int $bps_device
      * @return bool
      */
-    public static function set(string $c_blkio, int $diskSpeed):bool{
+    public static function set(string $c_blkio, int $primary_id, int $secondary_id, int $bps_device): bool
+    {
+        $fd = fopen($c_blkio . '/blkio.throttle.read_bps_device', 'w+');
+        fwrite($fd, $primary_id . ':' . $secondary_id . ' ' . $bps_device);
+        fclose($fd);
+        if (trim(file_get_contents($c_blkio . '/blkio.throttle.read_bps_device')) != $primary_id . ':' . $secondary_id . ' ' . $bps_device) {
+            return false;
+        }
+        $fd = fopen($c_blkio . '/blkio.throttle.write_bps_device', 'w+');
+        fwrite($fd, $primary_id . ':' . $secondary_id . ' ' . $bps_device);
+        fclose($fd);
+        if (trim(file_get_contents($c_blkio . '/blkio.throttle.write_bps_device')) != $primary_id . ':' . $secondary_id . ' ' . $bps_device) {
+            return false;
+        }
         return true;
     }
 }

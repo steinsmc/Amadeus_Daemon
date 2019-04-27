8d6ee26ff6f4b6dd039eaffe8a63b6e3a4d0cbac
William Wang <dhdjwjy@gmail.com>
2019-04-26 21:22:16 -0400

    Cgroup Cpu限制完成

--- src/Amadeus/Environment/Cgroup/Cpu.php
+++ src/Amadeus/Environment/Cgroup/Cpu.php
@@ -12,10 +12,24 @@ class Cpu
 {
     /**
      * @param string $c_cpu
-     * @param int $cpu
+     * @param int $rt_period_us
+     * @param int $rt_runtime_us
      * @return bool
      */
-    public static function set(string $c_cpu, int $cpu):bool{
+    public static function set(string $c_cpu, int $rt_period_us, int $rt_runtime_us): bool
+    {
+        $fd = fopen($c_cpu . '/cpu.rt_period_us', 'w+');
+        fwrite($fd, $rt_period_us);
+        fclose($fd);
+        if (trim(file_get_contents($c_cpu . '/cpu.rt_period_us')) != $rt_period_us) {
+            return false;
+        }
+        $fd = fopen($c_cpu . '/cpu.rt_runtime_us', 'w+');
+        fwrite($fd, $rt_runtime_us);
+        fclose($fd);
+        if (trim(file_get_contents($c_cpu . '/cpu.rt_runtime_us')) != $rt_runtime_us) {
+            return false;
+        }
         return true;
     }
 }

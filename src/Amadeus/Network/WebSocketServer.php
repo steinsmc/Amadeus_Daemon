491d3c2fd1ab5c2820399653f82c4917e2666877
William Wang <dhdjwjy@gmail.com>
2019-05-01 12:29:10 -0400

    增加了Amadeus自带的timer，这样就可以实现定时获取服务器状态了23333我真是聪明

--- src/Amadeus/Network/WebSocketServer.php
+++ src/Amadeus/Network/WebSocketServer.php
@@ -6,6 +6,8 @@ namespace Amadeus\Network;
 
 use Amadeus\Config\Config;
 use Amadeus\IO\Logger;
+use Amadeus\Process;
+use Swoole\Coroutine;
 use Swoole\WebSocket\Server as Server;
 
 /**
@@ -45,8 +47,29 @@ class WebSocketServer
         }
         $this->server = new Server($this->server_ip, $this->server_port, SWOOLE_BASE, SWOOLE_TCP);
         $this->server->set(array(
-            'worker_num' => $this->server_workers
+            'worker_num' => $this->server_workers,
+            'task_worker_num' => 2
         ));
+        $this->server->on('task', function ($server, $task_id, $from_id, $data) {
+            Logger::printLine('Swoole task worker started', Logger::LOG_SUCCESS);
+            if ($data == 'tick') {
+                while (file_exists(Process::getBase() . '/Amadeus.pid')) {
+                    Process::getPluginManager()->trigger('onServerTick');
+                    sleep(1);
+                }
+            }
+            $server->finish("{$data} -> finished");
+        });
+
+        $this->server->on('finish', function ($server, $task_id, $data) {
+            Logger::printLine('Swoole task worker stopped', Logger::LOG_INFORM);
+        });
+        $this->server->on('WorkerStart', function ($server, $workerId) {
+            if ($workerId == 0) {
+                $server->task('tick');
+                Logger::printLine('Amadeus timer started', Logger::LOG_SUCCESS);
+            }
+        });
         $this->server->on('open', ['Amadeus\Network\Reactor', 'onOpen']);
         $this->server->on('message', ['Amadeus\Network\Reactor', 'onMessage']);
         $this->server->on('close', ['Amadeus\Network\Reactor', 'onClose']);
